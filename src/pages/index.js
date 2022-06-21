b818e3b4196aa7ad9e194961c5a7585f136a5288
king tiffani <tiffanirengifo23@gmail.com>
2022-06-21 15:20:55 -0500

    learning-gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+
+// markup
+const IndexPage = () => {
+  return (
+   <div className="w-[200px] h-screen flex justify-center items-center mx-auto ">
+      <h1 className="bg-blue-500 text-white font-bol p-2">Hola Tiffani</h1>
+   </div>
+  )
+}
+
+export default IndexPage
